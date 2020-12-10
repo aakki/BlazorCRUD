855aca1d88d8511014c117b794364c3d2c5deb91
AnilVaghasiya <Anil.Vaghasiya@SIRVA.com>
2020-12-10 18:15:25 +0530

    Add project files.

---
+++ BlazorCRUD/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using BlazorCRUD.Shared.Appointment;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Appointment> tblAppointments { get; set; }
+
+    }
+}
---
+++ BlazorCRUD/Shared/Appointment/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Shared.Appointment
+{
+    public class Appointment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public string Problem { get; set; }
+        public DateTime CreatedDate { get; set; } = new DateTime();
+        public DateTime AppointmentDate { get; set; } = new DateTime();
+        public string Amount { get; set; }
+    }
+}
